9195eab00d155e0b3da7f27a1bdcc84a4accb6be
natoro <admin@natoro.com>
2020-08-22 11:47:13 +0900

    Initial commit

---
+++ app/src/main/java/io/tissue/study_20200822/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.tissue.study_20200822
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
